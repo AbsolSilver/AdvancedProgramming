1d278fe7edf97987ce7fa4c54088974b235b3e55
AbsolSilver <30814163+AbsolSilver@users.noreply.github.com>
2018-08-14 12:00:56 +1000

    Camera and player stuff

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 
     public GameObject weapon;
 
+    public Transform cam;
+
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
@@ -18,9 +20,13 @@ public class Player : MonoBehaviour
     void Update()
     {
         float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
 
         if (charC.isGrounded)
         {
+            Vector3 euler = cam.transform.eulerAngles;
+            transform.rotation = Quaternion.AngleAxis(euler.y, Vector3.up);
+
             moveDirection = new Vector3(inputH, 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class Orbit : MonoBehaviour
 {
 
+    public bool hideCursor = false;
+
     public Transform target;
-    public float distance = 5.0f;
-    public float xSpeed = 120.0f;
-    public float ySpeed = 120.0f;
+
+    public Vector3 offset = new Vector3(0, 1f, 0);
 
     public float yMinLimit = -20f;
     public float yMaxLimit = 80f;
@@ -18,45 +19,92 @@ public class Orbit : MonoBehaviour
     float x = 0.0f;
     float y = 0.0f;
 
+    [Header("Collision")]
+    public bool cameraCollision = false;
+    public float camRadius = 1f;
+    public float rayDistance = 1000f;
+    public LayerMask ignoreLayers;
+
+    private Vector3 originalOffset;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
     // Use this for initialization
     void Start()
     {
+        // Calculate offset of camera at start
+        originalOffset = transform.position - target.position;
+        // Ray distance is as long as the magnitude of offset
+        rayDistance = originalOffset.magnitude;
         transform.SetParent(null);
+
+        if (hideCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
 
         Vector3 angles = transform.eulerAngles;
         x = angles.y;
         y = angles.x;
+
+        transform.SetParent(null);
+    }
+
+    // Implement this OnDrawGizmos if you want to draw gizmos that are also pickable and always drawn
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, camRadius);
+    }
+
+
+
+    public void Look(float mouseX, float mouseY)
+    {
+        x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+        transform.rotation = rotation;
+    }
+
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            if (cameraCollision)
+            {
+                // Create ray that goes backwards from target
+                Ray camRay = new Ray(target.position, -transform.forward);
+                RaycastHit hit;
+                if (Physics.SphereCast(camRay, camRadius , out hit, rayDistance, ~ignoreLayers, QueryTriggerInteraction.Ignore))
+                {
+                    distance = hit.distance;
+                    // return - exit the function
+                    return;
+                }
+            }
+
+            // Reset distance if not hitting
+
+            distance = originalOffset.magnitude;
+        }
     }
 
     void LateUpdate()
     {
         if (target)
         {
-            x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
-            y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
-
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
-
-            Quaternion rotation = Quaternion.Euler(y, x, 0);
-
-            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
-
-            /*
-            RaycastHit hit;
-            if (Physics.Linecast(target.position, transform.position, out hit))
-            {
-                distance -= hit.distance;
-            }
-            */
-            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-            Vector3 position = rotation * negDistance + target.position;
-
-            transform.rotation = rotation;
-            transform.position = position;
-
-            Vector3 euler = transform.eulerAngles;
-            target.rotation = Quaternion.AngleAxis(euler.y, Vector3.up);
+            // Convert from world to local
+            Vector3 localOffset = transform.TransformDirection(offset);
+            transform.position = (target.position  + localOffset) + -transform.forward * distance;
         }
+
     }
 
     public static float ClampAngle(float angle, float min, float max)
